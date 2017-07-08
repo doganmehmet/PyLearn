3be4edf21539ea433139611a80017bca3104bfa5
Mehmet Dogan <mehmetdogan_07@windowslive.com>
2017-07-08 17:16:55 +0200

    example with all escape sequences

---
+++ Basics/LPHW/ex10.1.py
@@ -0,0 +1,18 @@
+# Using different escape sequences
+
+# check out more at https://docs.python.org/2.0/ref/strings.html
+
+print("I \\ love \\ python")
+print("I\'m Mehmet")
+print("I\"m again Mehmet")
+print("I said: I\am Mehmet")
+print("I said: I\bm Mehmet") # \b: backspace here deletes the I
+print("I said: I\fm Mehmet")
+# print("I said: I\N{blabla}m Mehmet") # look at the error
+print("I said I\rm Mehemt") # \r: Carriage return->deletes everything before \r
+print("I said: I\tm Mehmet") # puts a tab space
+print("I said: I\m Mehmet") # what it does?
+# print("I said: I\Um Mehmet") # why it gives error while the code one line aboe does not
+print("I said: I\vm Mehmet")
+# print("I said: I\ooom Mehmet") # \ooo how to use?
+# print("I said: I\xhh m Mehmet") # \xhh how to use?

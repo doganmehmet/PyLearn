33da35f0a27fd436b081eff85dc2cc9d0b2a4974
Mehmet Dogan <mehmetdogan_07@windowslive.com>
2017-07-08 16:46:27 +0200

    Add files via upload

---
+++ Basics/LPHW/ex9.py
@@ -0,0 +1,16 @@
+# PRINTING CON'T
+
+# definition of days and months variables. assign days of the week to days variable and months of the year to months months variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec" # \n: new line character. -> makes the next text to a new line
+
+# print days and months deifined above
+print(days)
+print(months)
+
+# print a text by using three double quotes to see what is going on
+print("""Check this out out!. Something
+is going on here with three double quotes.
+in this way we can write as much as  lines
+we want 2 lines, 3,4, even 133 lines. WoW!!!!!!
+""")

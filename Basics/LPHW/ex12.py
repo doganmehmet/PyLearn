2f1674c1827245e053046cebdbfb5715dcf2d8ce
Mehmet Dogan <mehmetdogan_07@windowslive.com>
2017-07-08 17:34:26 +0200

    getting input in a different way

---
+++ Basics/LPHW/ex12.py
@@ -0,0 +1,10 @@
+# GETTING INPUT FROM USER WITH A DIFFERENT WAY OF CODING
+
+# variable declarations and asking input from users
+name = input("What is your name?\n")
+surname = input("What is your surname?\n")
+weight = int(input("How much do you weight?\n"))
+""" Note: this time it does not go one line down automatically for the input. therefore we put new line character to go one line down. but of course this is a preference i might just give my input next to text or could use other escape sequences
+"""
+
+print(f"Your name is {name}, surname is {surname} and you have {weight} kg weight")
